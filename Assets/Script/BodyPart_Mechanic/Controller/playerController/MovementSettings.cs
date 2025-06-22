9d0d7dfa8dc5c98acdbba1498b08bcd8ccfda00a
Pupa-Tha <pupa.tha12@gmail.com>
2025-06-22 23:55:10 +0700

    KuyIsusMovement kyt

--- Assets/Script/BodyPart_Mechanic/Controller/playerController/MovementSettings.cs
+++ Assets/Script/BodyPart_Mechanic/Controller/playerController/MovementSettings.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+
+public enum SpeedMode
+{
+    Instant,
+    Accelerated
+}
+
 [CreateAssetMenu(fileName = "MovementSettings", menuName = "Leg/MovementSettings")]
 public class MovementSettings : ScriptableObject
 {
     [Header("Speeds")]
     public float maxSpeed = 5f;
     public float jumpForce = 5f;
+    public SpeedMode speedMode = SpeedMode.Accelerated;
 
     //no need to adjust those below value
 
@@ -14,6 +22,8 @@ public class MovementSettings : ScriptableObject
     [Header("Accel / Decel")]
     public float accelerationFactor = 5f;
     public float decelerationFactor = 10f;
+
+    public float groundSkin = 0.08f;
 
     [Header("Gravity")]
     public float gravity = -9.81f;
