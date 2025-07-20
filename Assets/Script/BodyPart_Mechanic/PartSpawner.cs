b74f815ebf111216bf5bb638e036f3f6002c3c7d
Suthanon <suthanonv@gmail.com>
2025-07-20 22:37:25 +0700

    Add QOL

--- Assets/Script/BodyPart_Mechanic/PartSpawner.cs
+++ Assets/Script/BodyPart_Mechanic/PartSpawner.cs
@@ -1,26 +1,17 @@
 using UnityEngine;
 
 public class PartSpawner : MonoBehaviour
-{        
-    [SerializeField] private Transform playerTransform; 
+{
+    [SerializeField] private Transform playerTransform;
 
     public void SpawnPart(Body bodySO)
     {
-        if(bodySO == null)
+        if (bodySO == null)
         {
             Debug.LogError("no bodyPart assign");
             return;
         }
 
-        GameObject prefab = bodySO.PartPrefabs;
-
-        if(prefab == null)
-        {
-            Debug.LogError("Prefab is not assigned in BodySo");
-            return;
-        }
-
-        Instantiate(prefab, playerTransform.position, playerTransform.rotation);
 
     }
 
