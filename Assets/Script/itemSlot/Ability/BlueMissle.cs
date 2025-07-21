77c108faa95b3d2ed3c45ee571f728240116ca04
Pupa-Tha <pupa.tha12@gmail.com>
2025-07-21 21:27:08 +0700

    4-NewAbility + improve missles

---
+++ Assets/Script/itemSlot/Ability/BlueMissle.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/Ability/BlueMissile")]
+public class BlueMissle : ItemAbility
+{
+    [Tooltip("Prefab with the HomingMissile component")]
+    public GameObject missilePrefab;
+
+    [Tooltip("How many to fire when used")]
+    public int missileCount = 1;
+
+    [Tooltip("Local offset from user when spawning")]
+    public Vector3 spawnOffset = new Vector3(0, 2, 0);
+
+    public override void Activate(GameObject user)
+    {
+        Vector3 origin = user.transform.position + spawnOffset;
+        Quaternion rot = user.transform.rotation;
+        for (int i = 0; i < missileCount; i++)
+        {
+            Instantiate(missilePrefab, origin, rot);
+        }
+        Debug.Log($"Spawned {missileCount} homing missiles");
+    }
+}
---
+++ Assets/Script/itemSlot/Ability/BlueMissleScript.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class BlueMissleScript : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed = 15f;
+    public float rotateSpeed = 6f;
+    public float lifeTime = 5f;
+
+    [Header("Collision")]
+    public float destroyDelay = 0.25f;
+
+    private Rigidbody rb;
+    private Transform target;
+    private Collider col;
+
+    private TrackUpdate[] players;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        Destroy(gameObject, lifeTime);  
+    }
+
+    void Start()
+    {
+        players = Object.FindObjectsByType<TrackUpdate>(FindObjectsSortMode.None);
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            TrackUpdate targetPlayer = null;
+            foreach (var player in players)
+            {
+                if (player.CurrentRank == 1)  
+                {
+                    targetPlayer = player;
+                    break;
+                }
+            }
+
+            if (targetPlayer != null)
+            {
+                target = targetPlayer.transform;
+                Debug.Log($"[Missile] Target acquired: {target.name}");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 dir = (target.position - transform.position).normalized;
+            Quaternion look = Quaternion.LookRotation(dir);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, look, rotateSpeed * Time.fixedDeltaTime));
+            rb.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ApplyStunEffect(other.gameObject);
+        }
+
+   
+
+        Destroy(gameObject, destroyDelay);
+    }
+
+ 
+    void ApplyStunEffect(GameObject player)
+    {
+        StatusManage statusManage = player.GetComponent<StatusManage>();
+        if (statusManage != null)
+        {
+            statusManage.OnApplyStatus(StatusType.Stun);  
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 5f); 
+    }
+}
