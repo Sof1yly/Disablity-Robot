cefe1f4040f40938c61daf845f07307a3b996ae5
Pupa-Tha <pupa.tha12@gmail.com>
2025-07-31 06:33:32 +0700

    fixTheFuck

--- Assets/Script/itemSlot/Ability/CallTeach.cs
+++ Assets/Script/itemSlot/Ability/CallTeach.cs
@@ -9,7 +9,6 @@ public class CallTeach : ItemAbility
     public int teacherCount = 1;
 
     public Vector3 spawnOffset = new Vector3(2, 6, 0);
-    [SerializeField] private AudioClip powerSound;
 
     public override void Activate(GameObject user)
     {
@@ -17,17 +16,8 @@ public class CallTeach : ItemAbility
         Quaternion rot = user.transform.rotation;
         for (int i = 0; i < teacherCount; i++)
         {
-            PlayThrowSound(user);
             Instantiate(teacherPrefab, origin, rot);
         }
     }
 
-    private void PlayThrowSound(GameObject user)
-    {
-        // Get the player's index to play the sound on the correct AudioSource
-        int playerIndex = user.GetComponentInChildren<TinyCarAudio>().player;
-
-        // Play the sound through the SoundPlayer
-        SoundPlayer.Instance.PlaySound(powerSound, playerIndex);
-    }
 }
--- Assets/Script/itemSlot/Ability/HomingMissileAbility.cs
+++ Assets/Script/itemSlot/Ability/HomingMissileAbility.cs
@@ -13,7 +13,6 @@ public class HomingMissileAbility : ItemAbility
     [Tooltip("Local offset from user when spawning")]
     public Vector3 spawnOffset = new Vector3(0, 1, 0);
 
-    [SerializeField] private AudioClip powerSound;
 
     public override void Activate(GameObject user)
     {
@@ -22,7 +21,6 @@ public class HomingMissileAbility : ItemAbility
         for (int i = 0; i < missileCount; i++)
         {
             GameObject missile = Instantiate(missilePrefab, origin, rot);
-            PlayThrowSound(user);
             HomingMissile missileScript = missile.GetComponent<HomingMissile>();
             if (missileScript != null)
             {
@@ -32,12 +30,4 @@ public class HomingMissileAbility : ItemAbility
         Debug.Log($"Spawned {missileCount} homing missiles");
     }
 
-    private void PlayThrowSound(GameObject user)
-    {
-        // Get the player's index to play the sound on the correct AudioSource
-        int playerIndex = user.GetComponentInChildren<TinyCarAudio>().player;
-
-        // Play the sound through the SoundPlayer
-        SoundPlayer.Instance.PlaySound(powerSound, playerIndex);
-    }
 }
