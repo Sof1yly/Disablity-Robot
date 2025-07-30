d759c345272ec19bd48583acc791d5f65266ef97
Pupa-Tha <pupa.tha12@gmail.com>
2025-07-31 02:37:17 +0700

    NewFood+ThrowFood

---
+++ Assets/Script/itemSlot/Ability/throwFood.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Inventory/Ability/ThrowFood")]
+public class throwFood : ItemAbility
+{
+    public override void Activate(GameObject target)
+    {
+        var statusManage = target.GetComponent<StatusManage>();
+        if (statusManage != null)
+        {
+            statusManage.OnApplyStatus(StatusType.VisualObscured);
+            
+        }
+    }
+}
